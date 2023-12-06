78d959f51b54185212ff2facd491d8691f2fef51
MX61LW <alain.vizzini@nnlife.co.jp>
2023-12-06 15:38:21 +0900

    Day6 part2 ok

---
+++ day6/decx.part2.py
@@ -0,0 +1,73 @@
+import io
+import sys
+import re
+
+
+
+def listmul(l):
+    r = 1
+    for i in l:
+        r = r * i
+    return r
+
+filename = "input.txt"
+llimit = -1
+if len(sys.argv) < 2:
+    print("INFO: no filename passed as param, defaulting to 'input.txt'")
+else:
+    print(f"INFO: 1st param is filename, will process '{sys.argv[1]}'")
+    filename = str(sys.argv[1])
+
+if len(sys.argv) >= 3:
+    print(f"INFO: 2nd param is limit of lines to process, will process only the first {sys.argv[2]} lines")
+    llimit = int(sys.argv[2])
+
+
+
+lcount = 0
+Times = []
+Distance = []
+with io.open(filename, "r") as f:
+    while True:
+        l = f.readline()
+        lcount = lcount +1
+
+        if not l:
+            #finished!
+            break
+
+        if llimit > 0 and lcount > llimit:
+            break
+
+        #process!
+        l = l.strip()
+        typ, l = l.split(":")
+
+        if typ.strip() == "Time":
+            l = l.replace(" ", "")
+            for m in re.findall(r'\d+', l):
+                Times.append(int(m))
+        
+        if typ.strip() == "Distance":
+            l = l.replace(" ", "")
+            for m in re.findall(r'\d+', l):
+                Distance.append(int(m))        
+
+
+#formula 
+# distance = (total_time - load_time) x load_time
+
+#let's go naive
+Results = []
+for i in range(len(Times)):
+    total_time = Times[i]
+    dist = Distance[i]
+
+    success = 0
+    for load_time in range(1, total_time):
+        if (total_time - load_time) * load_time > dist:
+            success = success + 1
+    Results.append(success)
+
+print(f"Results: {Results}")
+print(f"Result TOTAL: {listmul(Results)}")
